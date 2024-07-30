3c7045f926d4deaae242fd71c719a2c034772d1f
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-07-30 16:41:28 +0500

    Completed the polling functionality and added DMing functionality to notify the users when a poll ends

--- polling-bot.py
+++ polling-bot.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import os
 import time
+from datetime import datetime
+from datetime import timedelta
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,44 +26,100 @@ async def on_ready():
 
 
 @bot.command(name='poll')
-async def poll(ctx, question, *choices):
-    print(ctx.author.display_name)
+async def poll(ctx, question, hours, *choices):
+
+    try:
+        await ctx.message.delete()
+    except Exception as e:
+        await ctx.send(f"The bot faced an error!, please try again later!")
+        print(e)
+        return
+
     if len(choices) < 2:
         await ctx.send("You need to provide more choices for the poll.")
+        return
+    
     if len(choices) > 10:
         await ctx.send("You can only provide up to 10 choices for the poll.")
+        return
     
+    hours = float(hours)
+    endtime = datetime.now() + timedelta(hours=hours)
+    formatted_date = endtime.strftime("%d %B, %Y %I:%M%p").lower()
+
+    print(formatted_date)
+    print(ctx.author.display_name)
+
     emoji_numbers = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
     answers = []
+
     for i in range(len(choices)):
-        answers.append(f'{emoji_numbers[i]} - {choices[i]}')
-    
+        answers.append(f'{emoji_numbers[i]}\t-\t{choices[i]}')
+
     embed = discord.Embed(title=question, description="\n\n".join(answers), color=ctx.author.color)
-    embed.set_footer(text=f"Poll created by {ctx.author.name}.")
+    embed.set_footer(text=f"This poll will end at {formatted_date}\nPoll created by @{ctx.author.name}.")
+
     message = await ctx.send(embed=embed)
+
     for emoji in emoji_numbers[:len(choices)]:
         await message.add_reaction(emoji)
     
-    time.sleep(5)
+    try:
+        dm_channel = await ctx.author.create_dm()
+        embed = discord.Embed(
+                    title=f"Hi, @{ctx.author.name}!, I have successfully created a poll for you.\n", 
+                    description=f"Question:\n\t***{question}***\n\nOptions:\n\n"+"\n\n".join(answers), 
+                    color=ctx.author.color
+                )   
+        embed.set_footer(text=f"This poll will end at {formatted_date}\nPoll created by @{ctx.author.name}.")
+        await dm_channel.send(embed=embed)
+    except Exception as e:
+        print(f"Failed to send DM to {ctx.author.name}.") 
+        print(e)
+        await ctx.send(f"@{ctx.author.name} I can't send you a DM. Please check your DM settings.")
+
+    await asyncio.sleep(hours * 60 * 60) 
 
     newmessage = await ctx.fetch_message(message.id)
-    choices_ = {}
-    reactions = newmessage.reactions
-    result = "TIE"
-    for i in range(len(reactions)):
-        choices_[reactions[i].emoji] = reactions[i].count - 1
-    print(reactions)
+    choices_ = {reaction.emoji: reaction.count - 1 for reaction in newmessage.reactions}
 
-    if all_equal(choices_.values()):
-        result = "TIE"
+    print(newmessage.reactions)
+
+    if all(value == next(iter(choices_.values())) for value in choices_.values()):
+        result = "Result - The poll has ended in a tie between all options!"
     else:
         result = max(choices_, key=choices_.get)
+        winners = [key for key in choices_ if choices_[key] == choices_[result]]
+        if len(winners) == 1:
+            winner_index = emoji_numbers.index(winners[0])
+            result = f"Result - {choices[winner_index]} won the poll with {newmessage.reactions[emoji_numbers.index(winners[0])].count} votes."
+        elif len(winners) > 1:
+            result = f"Following options won the poll with same number of votes ({newmessage.reactions[emoji_numbers.index(winners[0])].count}):\n"
+            result += "\n\t".join(
+                [f"{winner}\t**{choices[emoji_numbers.index(winner)]}**" for winner in winners]
+            )    
 
     print(result)
 
-    embed = discord.Embed(title=question, description=f"Result: {result}", color=ctx.author.color)
-    embed.set_footer(text=f"Poll created by {ctx.author.name}.")
+    ending_at = datetime.now().strftime("%d %B, %Y %I:%M%p").lower()
+
+    embed = discord.Embed(title=question, description=result, color=ctx.author.color)
+    embed.set_footer(text=f"This poll ended at {ending_at}.\nPoll created by @{ctx.author.name}.")
+    
     await newmessage.edit(embed=embed)
 
+    try:
+        dm_channel = await ctx.author.create_dm()
+
+        embed = discord.Embed(
+                    title=f"Hi, @{ctx.author.name}!, Your poll has ended.\n", 
+                    description=f"Question:\n\t***{question}***\n\nOptions:\n\n"+"\n\n".join(answers)+f"\n\n{result}", 
+                    color=ctx.author.color
+                )   
+        embed.set_footer(text=f"This poll ended at {ending_at}\nPoll created by @{ctx.author.name}.")
+
+        await dm_channel.send(embed=embed)
+    except:
+        await ctx.send(f"@{ctx.author.name} I can't send you a DM. Please check your DM settings.")
 
 bot.run(token)
